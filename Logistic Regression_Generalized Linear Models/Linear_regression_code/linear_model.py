039df206686726b1adb71a1d684c8aba4e3a18c6
Gong Chuanyang <847714121@qq.com>
2020-12-17 21:29:11 +0800

    code - Logistic Regression Model (Finished) - without regularization

--- Logistic Regression_Generalized Linear Models/Linear_regression_code/linear_model.py
+++ Logistic Regression_Generalized Linear Models/Linear_regression_code/linear_model.py
@@ -1,7 +1,8 @@
 import numpy as np
 import struct
 
-
+###############################  start ###################################################
+#### 从同目录下的 dataset 中取出 Mnist 数据集部分内容
 def decode_idx3_ubyte(idx3_ubyte_file):
     """
     解析idx3文件的通用函数
@@ -153,6 +154,7 @@ def load_mnist(data_url = "dataset/"):
     
     return train_images, train_labels.reshape(-1,1), test_images, test_labels.reshape(-1,1)
 
+###############################  end  ###################################################
 
 def label_transform(labels, nums):
     """
@@ -192,3 +194,203 @@ def load_binary_dataset(data_url = "dataset/"):
     X_train, Y_train, X_dev, Y_dev = train_dev_split(X_train, Y_train, dev_ratio = dev_ratio)
     
     return X_train, Y_train.reshape(-1,1), X_dev, Y_dev.reshape(-1,1)
+
+def sigmoid(z):
+    """
+    计算输入的向量的 sigmoid 函数结果
+    """
+    return 1 / (1 + np.exp(-z))
+
+def normalization(X, train = True, X_mean = None, X_std = None):
+    """
+    X_mean 为 None 时表示传入的是训练集，进行归一化并返回 X_mean 和 X_std
+    X_mean 不为 None 时表示传入的是测试集，进行归一化
+    """
+    if(train):
+        flag = 0
+        X_mean = np.mean(X, 0).reshape(1, -1)
+        X_std  = np.std(X, 0).reshape(1,-1)
+        
+    X = (X - X_mean) / (X_std + 1e-8)
+    
+    if(train):
+        return X, X_mean, X_std
+    else:
+        return X
+    
+def shuffle(X, Y):
+    """
+    将输入的数据随机打乱
+    """
+    randomize = np.arange(X.shape[0])
+    np.random.shuffle(randomize)
+    return (X[randomize], Y[randomize])
+
+def split_to_miniBatch(X, Y, batchSize = 64):
+    """
+    将数据集按 batchSize 划分成多个子集，batchSize 默认值为 64
+    返回结果：一个 [] 列表，列表中每个元素为一个元组 ()，元组中包含两个参数 (X_miniBatch, Y_miniBatch)
+    """
+    X_shuffled, Y_shuffled = shuffle(X, Y)    #对样本进行随机打乱
+    m, n = X.shape                            #获取样本个数
+    completeBatchNum = int(np.ceil(m / batchSize)) #计算完整的 batch 个数
+    miniBatchs = []
+    
+    for i in range(completeBatchNum):
+        tempBatch_X = X[i * batchSize : (i + 1) * batchSize]
+        tempBatch_Y = Y[i * batchSize : (i + 1) * batchSize]
+        miniBatchs.append((tempBatch_X, tempBatch_Y))
+    
+    if(m % batchSize != 0):
+        tempBatch_X = X[completeBatchNum * batchSize :]
+        tempBatch_Y = Y[completeBatchNum * batchSize :]
+        miniBatchs.append((tempBatch_X, tempBatch_Y))
+    
+    return miniBatchs
+
+def add_col(X):
+    """
+    在输入向量的第一列前加一列全为 1 的列向量
+    """
+    m, n = X.shape
+    ones = np.ones((m, 1))
+    return np.hstack((ones, X))
+
+class LogisticRegressionModel:
+    def __init__(self, X, Y):
+        (temp_X, self.X_mean, self.X_std) = normalization(X)
+        self.Y = Y
+        self.X_train = add_col(temp_X)
+        self.m, self.n = self.X_train.shape
+        self.parameters = np.zeros((self.n, 1))
+        self.learning_rate = 0.00003
+        self.iteration = 10000
+        self.batchSize = 64
+        self.seed = 0
+        self.method = "batch"
+        self.printLossFlag = True
+        self.recordTimes = 0
+        self.recordInterval = 100
+        self.lossHistory = 0
+        self.regularizationFlag = False
+        self.accuracy = 0
+        self.trainFlag = False
+    
+    def modify_variables(self, learning_rate = None, iteration = None, method = None, batchSize = None, seed = None, printLossFlag = None, recordTimes = None, recordInterval = None, regularizationFlag = None):
+        if(learning_rate != None):
+            self.learning_rate = learning_rate
+        if(iteration != None):
+            self.iteration = iteration
+        if(method != None):
+            if(method == "batch" or method == "b"):
+                self.method = "batch"
+            elif(method == "miniBatch" or method == "mb"):
+                self.method = "miniBatch"
+            elif(method == "stochastic" or method == "s"):
+                self.method = "stochastic"
+        if(batchSize != None):
+            self.batchSize = batchSize
+        if(seed != None):
+            self.seed = seed
+        if(printLossFlag != None):
+            self.printLossFlag = printLossFlag
+        if(recordTimes != None):
+            self.recordTimes = recordTimes
+        if(recordInterval != None):
+            self.recordInterval = recordInterval
+        if(regularizationFlag != None):
+            self.regularizationFlag = regularizationFlag
+        
+    def hypothese(self, X, parameters):
+        return sigmoid(np.dot(X, parameters))
+    
+    def cal_gred(self, X, sub, regularizationFlag = False):
+        gred = np.dot(X.T, sub)
+        return gred
+    
+    def cal_loss(self, y, h, regularizationFlag = False):
+        loss = - np.dot(y.T, np.log(h + 1e-8)) - np.dot((1-y).T, np.log(1 - h + 1e-8))
+        return loss
+    
+    def train(self):
+        X = self.X_train
+        y = self.Y
+        train_method = self.method
+        m = self.m
+        iteration = self.iteration
+        learning_rate = self.learning_rate
+        batchSize = self.batchSize
+        printLossFlag = self.printLossFlag
+        regularizationFlag = self.regularizationFlag
+        
+        
+        if(train_method == "miniBatch"):   #判断是否为 miniBatch ，并划分子集
+            miniBatchs = split_to_miniBatch(X, y, batchSize)
+            batchNum = len(miniBatchs)
+    
+        if (train_method == "miniBatch"):  #计算内循环次数
+            subIteration = batchNum
+        elif (train_method == "stochastic"):
+            subIteration = m
+        else:
+            subIteration = 1
+        
+        if(int(self.recordTimes) > 0):    #计算记录间隔
+            recordInterval = int(np.floor(iteration * subIteration / self.recordTimes))
+        else:
+            recordInterval = int(self.recordInterval)
+        
+        loss_history = np.zeros((int(np.floor(iteration * subIteration / recordInterval)),1))
+        loss_i = 0
+        
+        for ite in range(iteration):
+            for subIte in range(subIteration):
+                parameters = self.parameters
+                if (train_method == "miniBatch"):    #取出当前循环需要用于计算的 batch
+                    batch_X, batch_y = miniBatchs[subIte]
+                elif (train_method == "stochastic"):
+                    batch_X, batch_y = (X[subIte, : ].reshape(1 , -1), y[subIte].reshape(-1 , 1))
+                else:
+                    batch_X, batch_y = (X, y)
+                    
+                h = self.hypothese(batch_X, parameters)
+                sub = h - batch_y
+                gred = self.cal_gred(batch_X, sub, regularizationFlag)
+            
+                if(printLossFlag and (ite * subIteration + subIte) % recordInterval == 0):
+                    X_h = self.hypothese(X, parameters)
+                    loss_history[loss_i] = self.cal_loss(y, X_h, regularizationFlag)
+                    print("iteration "+ str(ite * subIteration + subIte) +" loss: " +  str(loss_history[loss_i]))
+                    loss_i = loss_i + 1
+                self.parameters = self.parameters - learning_rate * gred
+        pre = self.predict(X)
+        accuracy = self._accuracy(pre, y)
+        if(printLossFlag):
+            print("Accuracy: " + str(accuracy))
+        self.accuracy = accuracy
+        self.trainFlag = True
+                
+    def initial_parameters(self):
+        self.parameters = np.zeros((self.n, 1))
+    
+    def predict(self, X):
+        if(X.shape[1] != self.n):
+            X = add_col(normalization(X, train = False, X_mean = self.X_mean, X_std = self.X_std))
+        h = self.hypothese(X, self.parameters)
+        return np.round(h).astype(np.int)
+    
+    def _accuracy(self, Y_pred, Y_label):
+        # This function calculates prediction accuracy
+        acc = 1 - np.mean(np.abs(Y_pred - Y_label))
+        return acc
+    
+    def print_info(self):
+        print("Train data shape: ("+ str(self.m) +", "+ str(self.n)+ ")")
+        print("learning_rate: " + str(self.learning_rate))
+        print("iteration: " + str(self.iteration))
+        print("trainFlag: " + str(self.trainFlag))
+        print("accuracy: " + str(self.accuracy))
+        print("regularizationFlag: " + str(self.regularizationFlag))
+        print("printLossFlag: " + str(self.printLossFlag))
+        print("method: " + str(self.method))
+        print("batchSize: " + str(self.batchSize))
